dd4a66087311a811e2e7bc7e57ba838a14c567c4
akshaya0537-coder <akshaya0537@gmail.com>
2025-09-30 06:52:47 +0000

    Sync LeetCode submission Runtime - 22 ms (61.60%), Memory - 19.9 MB (100.00%)

---
+++ 0455-assign-cookies/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        child_idx = 0
+        cookie_idx = 0
+        while child_idx < len(g) and cookie_idx < len(s):
+            if s[cookie_idx] >= g[child_idx]:
+                child_idx += 1
+                cookie_idx += 1
+            else:
+                cookie_idx += 1
+        return child_idx
