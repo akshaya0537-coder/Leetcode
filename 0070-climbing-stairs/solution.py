d574d07e2b9a27e8f583b81c7567bf016fc5cbb0
akshaya0537-coder <akshaya0537@gmail.com>
2025-10-26 04:25:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.9 MB (100.00%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -3,6 +3,6 @@ class Solution:
         d=[0]*(n+1)
         d[0]=1
         d[1]=1
-        for i in range(2,n+1):
+        for i in range (2,n+1):
             d[i]=d[i-1]+d[i-2]
         return d[-1]
